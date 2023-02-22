7f2f15d668182850b108525bac843d46191b5884
Priya Mourya <priya.mourya1999@gmail.com>
2023-02-22 21:39:54 +0530

    processing and transformation

--- src/main/java/com/springproject/camelmicroservicea/routes/a/MyFirstTimerRouter.java
+++ src/main/java/com/springproject/camelmicroservicea/routes/a/MyFirstTimerRouter.java
@@ -1,24 +1,49 @@
 package com.springproject.camelmicroservicea.routes.a;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+
 
 @Component
 public class MyFirstTimerRouter extends RouteBuilder {
+    @Autowired
+    private GetCurrentTimeBean getCurrentTimeBean;
+    @Autowired
+    private SimpleLoggingProcessingComponent loggingComponent;
     @Override
     public void configure() throws Exception {
         //queue- use timer
         //transformation
         //database -log
         //Exchange[ExchangePattern: InOnly, BodyType: null, Body: [Body is null]]
+
+        //processing- donot change body of message
+        //transformation- changes body of message
+
         from("timer:first-timer")//start of queue
-                .transform().constant("My Constant Message")
-                .transform().constant(LocalDateTime.now())
+                //.transform().constant("My Constant Message")
+                .bean(getCurrentTimeBean,"getCurrentTime")
+                .bean(loggingComponent)
                 .to("log:first-timer");//database
 
 
+    }
+}
+
+@Component
+class GetCurrentTimeBean {
+    public String getCurrentTime() {
+        return "Time now is " + LocalDateTime.now();
+    }
+}
+@Component
+class SimpleLoggingProcessingComponent {
+    private Logger logger= (Logger) LoggerFactory.getLogger(SimpleLoggingProcessingComponent.class);
+    public void process(String message) {
+        logger.info("SimpleLoggingProcessComponent {}",message);
     }
 }
