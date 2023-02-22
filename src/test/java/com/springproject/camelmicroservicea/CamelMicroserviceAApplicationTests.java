f92b979e43e1693c3e9492c878ac089b3c981a52
Priya Mourya <0201cs181063@gmail.com>
2023-02-22 20:04:44 +0530

    Adding project

---
+++ src/test/java/com/springproject/camelmicroservicea/CamelMicroserviceAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.camelmicroservicea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
